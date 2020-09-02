4166a120532f2574d2afedcb09739f41e0b322ec
devdan <61592212+devdan254@users.noreply.github.com>
2020-09-02 15:33:28 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Chat App</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="chat-container">
+
+	<div id="search-container">
+		<input type="text" name="" placeholder=" search">
+		
+	</div>
+	<div id="conversation-list">
+		<div class="conversation active ">
+			<img src="images/male.png" >
+			<div class="title-text">
+				Daniel Miheso
+			</div>
+			<div class="created-date">
+				Apr 16
+			</div>
+			<div class="conversation-message">
+				This is the message 
+		</div>
+	</div>
+		<div class="conversation  ">
+			<img src="images/male.png" >
+			<div class="title-text">
+				Daniel Miheso
+			</div>
+			<div class="created-date">
+				Apr 16
+			</div>
+			<div class="conversation-message">
+				This is the message
+			</div>
+		</div>
+		<div class="conversation  ">
+			<img src="images/male.png" >
+			<div class="title-text">
+				Daniel Miheso
+			</div>
+			<div class="created-date">
+				Apr 16
+			</div>
+			<div class="conversation-message">
+				This is the message
+			</div>
+		</div>
+		<div class="conversation  ">
+			<img src="images/male.png" >
+			<div class="title-text">
+				Daniel Miheso
+			</div>
+			<div class="created-date">
+				Apr 16
+			</div>
+			<div class="conversation-message">
+				This is the message
+			</div>
+		</div>
+		<div class="conversation  ">
+			<img src="images/male.png" >
+			<div class="title-text">
+				Daniel Miheso
+			</div>
+			<div class="created-date">
+				Apr 16
+			</div>
+			<div class="conversation-message">
+				This is the message
+			</div>
+		</div>
+		<div class="conversation ">
+			<img src="images/male.png" >
+			<div class="title-text">
+				Daniel Miheso
+			</div>
+			<div class="created-date">
+				Apr 16
+			</div>
+			<div class="conversation-message">
+				This is the message
+			</div>
+		</div>
+		<div class="conversation  ">
+			<img src="images/male.png" >
+			<div class="title-text">
+				Daniel Miheso
+			</div>
+			<div class="created-date">
+				Apr 16
+			</div>
+			<div class="conversation-message">
+				This is the message
+			</div>
+		</div>
+		
+	
+</div>
+	
+
+	<div id="new-message-container">
+		<a href="#">+</a>
+	</div>
+	<div id="chat-title">
+		<span>Dann Luyali</span>
+		<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAAyElEQVRIie2VMQrCQBBF34REe8HC0ksJZm+QTaG2gr1YKcQbJDcT7Cy0UBDHxogSE2NURMivdhnmv/2zCys8URTHITgj0O5dQVhxYhGY/kxENK9fCs2TJECJio+gA2vMPK/qFPfKEEDRsbvbNqzxxRpf2p7rgk4uFmGRxTVBFCe5MavIGl/gWYJ/UOaS3x6V53Zsr7dOt18f0W8B6Usou34Z8AnVgBpQA2pACT36D/ZAs7qhtgJjNuk+k0CVKXCo4H1EWd6aA5wBW281+FbPBcAAAAAASUVORK5CYII=">
+	</div>
+
+	<div id="chat-message-list">
+		<div class="message-row you-message">
+			<div class="message-content">
+			<div class="message-text">Ok then</div>
+			<div class="message-time">Apr 16</div>
+		</div>
+	</div>
+		<div class="message-row other-message">
+			<div class="message-content">
+			<img src="images/female.png" style="width: 60px;height: 60px;">
+			<div class="message-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua.</div>
+			<div class="message-time">Apr 16</div>
+		</div>
+		</div>
+		<div class="message-row you-message">
+			<div class="message-content">
+			<div class="message-text">Ok then</div>
+			<div class="message-time">Apr 16</div>
+		</div>
+	</div>
+		<div class="message-row other-message">
+			<div class="message-content">
+			<img src="images/female.png" style="width: 60px;height: 60px;">
+			<div class="message-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua.</div>
+			<div class="message-time">Apr 16</div>
+		</div>
+		</div>
+		<div class="message-row you-message">
+			<div class="message-content">
+			<div class="message-text">Ok then</div>
+			<div class="message-time">Apr 16</div>
+		</div>
+	</div>
+		<div class="message-row other-message">
+			<div class="message-content">
+			<img src="images/female.png" style="width: 60px;height: 60px;">
+			<div class="message-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua.</div>
+			<div class="message-time">Apr 16</div>
+		</div>
+		</div>
+		<div class="message-row you-message">
+			<div class="message-content">
+			<div class="message-text">Ok then</div>
+			<div class="message-time">Apr 16</div>
+		</div>
+	</div>
+		<div class="message-row other-message">
+			<div class="message-content">
+			<img src="images/female.png" style="width: 60px;height: 60px;">
+			<div class="message-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua.</div>
+			<div class="message-time">Apr 16</div>
+		</div>
+		</div>
+		<div class="message-row you-message">
+			<div class="message-content">
+			<div class="message-text">Ok then</div>
+			<div class="message-time">Apr 16</div>
+		</div>
+	</div>
+		<div class="message-row other-message">
+			<div class="message-content">
+			<img src="images/female.png" style="width: 60px;height: 60px;">
+			<div class="message-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua.</div>
+			<div class="message-time">Apr 16</div>
+		</div>
+		</div>
+		<div class="message-row you-message">
+			<div class="message-content">
+			<div class="message-text">Ok then</div>
+			<div class="message-time">Apr 16</div>
+		</div>
+	</div>
+		<div class="message-row other-message">
+			<div class="message-content">
+			<img src="images/female.png" style="width: 60px;height: 60px;">
+			<div class="message-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua.</div>
+			<div class="message-time">Apr 16</div>
+		</div>
+		</div>
+	</div>
+	<div id="chat-form"> 
+		<img src="https://img.icons8.com/fluent/48/000000/attach.png"/>
+		<input type="text" name="" placeholder="type a message">
+	</div>
+   
+</div>	
+
+
+</body>
+</html>
